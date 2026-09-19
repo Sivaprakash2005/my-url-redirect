b7cad15fa9fa60756f516d36ab600abc3be060f8
Sivaprakash2005 <141418410+Sivaprakash2005@users.noreply.github.com>
2026-09-19 12:20:02 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,20 +2,29 @@ from flask import Flask, redirect
 
 app = Flask(__name__)
 
-SOURCE_PATH = "/hUtZh0"
+# YOUR DOMAIN
+DOMAIN = "qrto.org"
 
+# DESTINATION
 DESTINATION_URL = "https://docs.google.com/forms/d/e/1FAIpQLSeYrUBMQVCKx3tY058wU1aDxJzBLbbMPwRcdu_996lW-Kj7iw/viewform"
 
 
-@app.route(SOURCE_PATH)
-def redirect_site():
-    return redirect(DESTINATION_URL)
+@app.route("/hUtZh0")
+def activate_redirect():
+
+    print(f"{DOMAIN}/hUtZh0 activated")
+
+    return redirect(DESTINATION_URL, code=302)
 
 
 @app.route("/")
 def home():
-    return "URL Redirect is working!"
+    return f"""
+    <h1>{DOMAIN}</h1>
+    <p>Redirect is active.</p>
+    <a href="/hUtZh0">Open Link</a>
+    """
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
